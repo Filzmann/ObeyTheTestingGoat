f9940ae766b1acaf611e1f5ebbae066f2faaa399
Simon Beyer <simonbeyer79@gmail.com>
2021-08-17 16:24:06 +0200

    Front page HTML now generated from a template

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,4 +1,4 @@
-from django.utils.datetime_safe import time
+import time
 from selenium import webdriver
 import unittest
 
@@ -42,7 +42,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. She
@@ -52,13 +53,14 @@ class NewVisitorTest(unittest.TestCase):
 
     # The page updates again, and now shows both items on her list
 
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
+    # Edith wonders whether the site will remember her list. Then she sees
+    # that the site has generated a unique URL for her -- there is some
+    # explanatory text to that effect.
 
-        # She visits that URL - her to-do list is still there.
+    # She visits that URL - her to-do list is still there.
 
-        # Satisfied, she goes back to sleep
+    # Satisfied, she goes back to sleep
+
 
 if __name__ == '__main__':
     unittest.main()
