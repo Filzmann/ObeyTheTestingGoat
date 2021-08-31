a8e859f926b194c7f0152bff7c772c0e693d24d4
Simon Beyer <simonbeyer79@gmail.com>
2021-08-31 16:29:08 +0200

    URLs for forms x2, moved code in views + tests, new UR

--- lists/views.py
+++ lists/views.py
@@ -5,9 +5,6 @@ from lists.models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request=request, template_name='home.html')
 
 
@@ -18,3 +15,8 @@ def view_list(request):
         template_name='list.html',
         context={'items': items}
     )
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
