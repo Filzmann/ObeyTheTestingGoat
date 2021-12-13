52e210f5b34f6b51c336673ee9a79a1035d73768
Simon Beyer <simonbeyer79@gmail.com>
2021-12-13 11:34:25 +0100

    gitignore .env file

--- superlists/settings.py
+++ superlists/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'superliststs.urls'
+ROOT_URLCONF = 'superlists.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'superliststs.wsgi.application'
+WSGI_APPLICATION = 'superlists.wsgi.application'
 
 
 # Database
